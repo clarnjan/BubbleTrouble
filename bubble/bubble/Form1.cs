72b1ac67768e2bbc1126c5bc5e5c1325c3b90388
Damjan Anakiev <damjananakiev@gmail.com>
2019-05-18 17:09:42 +0200

    Bubble movement

---
+++ bubble/bubble/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bubble
+{
+    public partial class Form1 : Form
+    {
+        Bubble topche;
+        Bubble topche1;
+        Bubble topche2;
+        public Form1()
+        {
+            InitializeComponent();
+            topche = new Bubble(100, 100, 80, Color.Blue);
+            topche1 = new Bubble(600, 100, 40, Color.Blue);
+            topche2 = new Bubble(300, 100, 10, Color.Blue);
+            DoubleBuffered = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            topche.Draw(e.Graphics);
+            topche1.Draw(e.Graphics);
+            topche2.Draw(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            topche.Move(Width);
+            topche.Gravity(Height);
+            topche1.Move(Width);
+            topche1.Gravity(Height);
+            topche2.Move(Width);
+            topche2.Gravity(Height);
+            Invalidate();
+        }
+    }
+}
---
+++ bubble/bubble/bubble.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubble
+{
+    class Bubble
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public int radius { get; set; }
+        public bool nasoka { get; set; }
+        public bool bounce { get; set; }
+        public double  zabrzuvanje { get; set; }
+        public Color boja { get; set; }
+        
+        public Bubble(int xx,int yy,int r,Color c)
+        {
+            x = xx;
+            y = yy;
+            radius = r;
+            boja = c;
+            nasoka = true;
+            bounce = true;
+            zabrzuvanje = 0;
+        }
+        public void Draw(Graphics g)
+        {
+            Brush chetka = new SolidBrush(boja);
+            g.FillEllipse(chetka, (int)x - radius, (int)y - radius, radius * 2, radius * 2);
+        }
+
+        public void Move(int width)
+        {
+            if (nasoka)
+            {
+                x += 4;
+                if (x + radius >= width - 15)
+                {
+                    nasoka = false;
+                }
+            }
+            else
+            {
+                x -= 4;
+                if (x - radius <= 0)
+                {
+                    nasoka = true;
+                }
+            }
+        }
+        public void Gravity(int height)
+        {
+            if (bounce)
+            {
+                y += 0.5*zabrzuvanje;
+                zabrzuvanje +=1;
+                if (y + radius >= height - 40 )
+                {
+                    zabrzuvanje = 2*Math.Sqrt(radius)+20;
+                    bounce = false;
+                }
+            }
+            else
+            {
+                y -= 0.5 * zabrzuvanje;
+                zabrzuvanje -= 1 ;
+                if (zabrzuvanje<=0)
+                {
+                    bounce = true;
+                }
+            }
+        }
+    }
+}
